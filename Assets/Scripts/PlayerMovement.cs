feb2409595123234a6b872958c2c7f0b4a5a8b91
duyanh14 <duyanhnguyen2001@gmail.com>
2022-06-28 02:25:02 +0700

    Frist commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v =  Input.GetAxis("Vertical");
+        
+        rb.position = (transform.position + new Vector3(h,v) * 6f * Time.deltaTime);
+    }
+}
